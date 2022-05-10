85cd405cdee87020156cb3ab1095ed49caafaa78
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-10 17:34:35 +0300

    ft:email configurations and general configurations update

--- config.py
+++ config.py
@@ -1,14 +1,59 @@
-from distutils.debug import DEBUG
 import os
-from dotenv import load_dotenv, find_dotenv
 
-
-load_dotenv(find_dotenv())
-
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') # 'postgresql://localhost/postgres'
-    DEBUG = os.getenv('DEBUG')
+class Config: # Config class
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    #SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:kipkemoi2000@localhost/oneminute_pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
-    breakpoint()
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitches!'
+    SENDER_EMAIL = 'kipkemoilvs@gmail.com'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Test  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kipkemoi:password@localhost/oneminute_pitches_test'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:kipkemoi2000@localhost/oneminute_pitches'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
