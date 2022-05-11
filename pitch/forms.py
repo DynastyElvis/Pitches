97df1c923d9d7e1f5b5e4e29e4ec2d6653f81085
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 18:04:06 +0300

    ft: user validation, request forms and reset passwords

--- pitch/forms.py
+++ pitch/forms.py
@@ -33,3 +33,47 @@ class LoginForm(FlaskForm):
    
     submit = SubmitField('Login')
     
+    
+class UpdateForm(FlaskForm):
+    username = StringField('First Name', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submit = SubmitField('Update Profile')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Name is Already Taken, Please use another name!')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already taken, Please use another email!')
+            
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[DataRequired()])
+    category = SelectField('Pitch Category', choices=[('Business','Business'),('Finance','Finance'),('Relationships','Relationships'),('Well-Being','Well-Being')],validators=[DataRequired()])
+    content = TextAreaField('Pitch Content', validators=[DataRequired()])
+    submit = SubmitField('Pitch')
+    
+class CommentForm(FlaskForm):
+    
+    userdata = TextAreaField('Comments...',validators=[DataRequired()])
+    submit = SubmitField('Post')
+    
+class RequestResetForm(FlaskForm):
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('No account exists with this email! Register first!!')
+        
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
