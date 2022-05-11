0c891664afff681008d54150a54b543f5232b204
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 18:14:56 +0300

    bg: model update

--- pitch/models.py
+++ pitch/models.py
@@ -33,3 +33,21 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+       
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    category = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+   
+    
+    def __repr__(self):
+        return f"Pitch('{self.title}', '{self.date_posted}')"
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userdata = db.Column(db.Text, nullable=False )
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'), nullable=True)
+    
