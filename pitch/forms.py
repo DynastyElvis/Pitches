ee7bc48989d03c87dd3a9d0c57415db8f4d09d75
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 18:02:52 +0300

    ft: Registration and login forms logic

---
+++ pitch/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from pitch.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('First Name', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That Name is Already Taken, Please use another name!')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken, Please use another email!')
+    
+
+class LoginForm(FlaskForm):
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    
+    remember = BooleanField('Remember me')
+    
+   
+    submit = SubmitField('Login')
+    
