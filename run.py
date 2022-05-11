9baf2266a2a360f11a59dd84f59f6af74a5d01ae
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 16:05:00 +0300

    ft: test server

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from pitch import app
+#from pitch import app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 
-
-if __name__ == '__main__':
-    app.run(debug = True)
+#if __name__ == '__main__':
+ #   app.run(debug = True)
