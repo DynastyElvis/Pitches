7d35010c5eb5c3f4f41e0d9f4727226026bc4da0
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 17:45:40 +0300

    bg: fixed bugs

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-#from pitch import app
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+from pitch import app
 
 
-#if __name__ == '__main__':
- #   app.run(debug = True)
+
+if __name__ == '__main__':
+    app.run(debug = True)
