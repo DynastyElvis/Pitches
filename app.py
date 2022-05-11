311217664b2579bd239f421da60f855abe2940c5
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-11 15:37:34 +0300

    bg: run.py logic

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask#, render_template
+#from flask import Flask#, render_template
 
-app = Flask(__name__)#, template_folder='templates')
+#app = Flask(__name__)#, template_folder='templates')
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+#@app.route('/')
+#def home():
+#   return "Hello, World!"
 
 
 
